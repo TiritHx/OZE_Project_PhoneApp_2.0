7a6ddbd3bec02ed667d6f6856b165fcceb96001e
Mikku <90571443+MaciejMikulskii@users.noreply.github.com>
2023-04-17 13:46:02 +0200

    bezkresneentry

---
+++ PlainEntry.cs
@@ -0,0 +1,12 @@
+﻿// zrob folder o nazwie Effects i umiesc tam ten plik
+using Xamarin.Forms;
+
+namespace OZE_2._0.Effects
+{
+    public class PlainEntry : RoutingEffect
+    {
+        public PlainEntry() : base("PlainEntryGroup.PlainEntryEffect")
+        {
+        }
+    }
+}
