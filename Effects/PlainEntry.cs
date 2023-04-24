3b973329c408c50b4394d2bfbd30181d58b832cf
Dominik <90091417+TiritHx@users.noreply.github.com>
2023-04-24 11:54:40 +0200

    Added effects
    Maciek ma skleroze itd

---
+++ Effects/PlainEntry.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace OZE_2._0.Effects
+{
+    public class PlainEntry : RoutingEffect
+    {
+        public PlainEntry() : base("PlainEntryGroup.PlainEntryEffect")
+        {
+        }
+    }
+}
