22c28f9e9221d8884a9b32eea0bb11fd509a46ae
Dominik <90091417+TiritHx@users.noreply.github.com>
2023-05-15 13:54:24 +0200

    git merż dont care 2.0
    UWAGA PLIKI: MainActivity.cs, AndroidPlainEntry.cs i folder drawable jest do androida 🤯🤯🤯🤯🤯😬😬🥶😫🤔🤔🤔🤔😎

--- MainActivity.cs
+++ MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace OZE_2._0.Droid
 {
-    [Activity(Label = "OZE_2._0", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "OZE_2._0", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
